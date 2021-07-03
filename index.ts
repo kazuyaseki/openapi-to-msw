3314eddbe7ae888cf552a9daef1dbe110e8efd17
kazuyaseki <k.seki@kakehashi.life>
2021-07-03 10:16:53 +0900

    add typings for openapi

--- index.ts
+++ index.ts
@@ -1,9 +1,29 @@
 import { load } from "js-yaml";
 import { readFileSync } from "fs";
 
-function generate(json: any) {
-  console.log(json);
+type Response = {};
+
+type OpenAPI = {
+  openapi: string;
+  info: { title: string; version: string };
+  paths: {
+    [key: string]: {
+      [key in "get" | "post" | "put" | "delete"]: {
+        description: string;
+        responses: { [key: string]: any };
+      };
+    };
+  };
+  components: {
+    schemas: any;
+  };
+};
+
+function generate(doc: OpenAPI) {
+  Object.values(doc.paths).forEach((value) => {
+    console.log(value);
+  });
 }
 
-const doc = load(readFileSync("./test-openapi.yml", "utf8"));
+const doc = load(readFileSync("./test-openapi.yml", "utf8")) as OpenAPI;
 generate(doc);
